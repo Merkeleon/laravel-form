5ee77f5418a735c887519b6a1ba8e1a355623719
Andrey Drozd <andrey@drozd.by>
2017-07-28 15:18:54 +0300

    fix error classes

--- src/resources/views/default/element/password.blade.php
+++ src/resources/views/default/element/password.blade.php
@@ -1,4 +1,4 @@
-<div class="form__element form__element_type_password @if($error) form__element_error @endif {{$class}}">
+<div class="form__element form__element_type_password @if($error) form__element--error @endif {{$class}}">
     <label class="form__label">{{ $label }}</label>
     <div class="form__element-container">
         <input @foreach($attributes as $attributeName => $attributeValue) {{ $attributeName }}="{{ $attributeValue }}" @endforeach type="password" name="{{ $elementName }}" placeholder="{{ $placeholder }}">
